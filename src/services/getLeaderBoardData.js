d7e03c79b57923634787f2600209665ff6dafcd8
batubb <batuhanberkbalci@gmail.com>
2021-06-08 21:56:14 -0400

    Prevent leaderboard and trendings to render my own profile

--- src/services/getLeaderBoardData.js
+++ src/services/getLeaderBoardData.js
@@ -1,39 +1,49 @@
 /* eslint-disable prettier/prettier */
 import database from '@react-native-firebase/database';
-import { constants } from '../resources';
+import {constants} from '../resources';
+import Store from '../store/Store';
 
-export default async function getLeaderBoardData(uid = '', limit = constants.DEALS_LIMIT) {
-    const value = await database().ref('users').orderByChild('follower').once('value');
+export default async function getLeaderBoardData(
+  curUid = Store.uid,
+  limit = constants.DEALS_LIMIT,
+) {
+  const value = await database()
+    .ref('users')
+    .orderByChild('follower')
+    .once('value');
 
-    var x = 1;
-    var userType = 'Follower';
-    var leaderBoardArray = [];
+  var userType = 'Follower';
+  var leaderBoardArray = [];
 
-    value.forEach(element => {
-        const { type } = element.val();
+  value.forEach((element) => {
+    const {type, uid} = element.val();
 
-        if (type === 'user') {
-            userType = 'Follower';
-        } else if (type === 'influencer') {
-            userType = 'Influencer';
-        }
-
-        leaderBoardArray.push({ ...element.val(), userType: userType });
-        x++;
-    });
-
-    leaderBoardArray.reverse();
-    
-    leaderBoardArray.slice(0, 20);
-
-    var newleaderBoardArray = [];
-
-    for (let i = 0; i < leaderBoardArray.length; i++) {
-        const element = leaderBoardArray[i];
-        newleaderBoardArray.push({ ...element, index: i + 1 });
+    if (curUid === uid) {
+      return;
     }
 
-    const result = newleaderBoardArray.filter(user => user.type === 'influencer');
+    if (type === 'user') {
+      userType = 'Follower';
+    } else if (type === 'influencer') {
+      userType = 'Influencer';
+    }
 
-    return result;
+    leaderBoardArray.push({...element.val(), userType: userType});
+  });
+
+  leaderBoardArray = leaderBoardArray.filter(
+    (user) => user.type === 'influencer',
+  );
+  leaderBoardArray.reverse();
+
+  leaderBoardArray.slice(0, 20);
+
+  var newleaderBoardArray = [];
+
+  for (let i = 0; i < leaderBoardArray.length; i++) {
+    const element = leaderBoardArray[i];
+    newleaderBoardArray.push({...element, index: i + 1});
+  }
+
+  return newleaderBoardArray;
 }
--- src/services/getTrendingsData.js
+++ src/services/getTrendingsData.js
@@ -1,15 +1,20 @@
 /* eslint-disable prettier/prettier */
 import database from '@react-native-firebase/database';
+import Store from '../store/Store';
 
-export default async function getTrendingsData(uid = '') {
-    const value = await database().ref('users').orderByChild('type').equalTo('influencer').once('value');
+export default async function getTrendingsData(uid = Store.uid) {
+  const value = await database()
+    .ref('users')
+    .orderByChild('type')
+    .equalTo('influencer')
+    .once('value');
 
-    var trendingsArray = [];
-    value.forEach(element => {
-        if (uid !== element.val().uid) {
-            trendingsArray.push(element.val());
-        }
-    });
+  var trendingsArray = [];
+  value.forEach((element) => {
+    if (uid !== element.val().uid) {
+      trendingsArray.push(element.val());
+    }
+  });
 
-    return trendingsArray;
+  return trendingsArray;
 }
