afe3cbe2537bf7ac1d7a1fdf9c3ae306c3045805
batubb <batuhanberkbalci@gmail.com>
2021-06-14 23:29:33 -0400

    Change formatting of setVideoView

--- src/services/setVideoView.js
+++ src/services/setVideoView.js
@@ -2,20 +2,27 @@
 import database from '@react-native-firebase/database';
 
 export default async function setVideoView(uid, video) {
-    const data = {
-        timestamp: new Date().getTime(),
-        uid: uid,
-        video: video.uid,
-        viewed: true,
-    };
+  const data = {
+    timestamp: new Date().getTime(),
+    uid: uid,
+    video: video.uid,
+    viewed: true,
+  };
 
-    await database().ref('views').child(video.uid).child(uid).set(data);
+  await database().ref('views').child(video.uid).child(uid).set(data);
 
-    const videoViewData = await database().ref('views').child(video.uid).once('value');
-    const viewCount = (Object.keys(videoViewData.val())).length;
+  const videoViewData = await database()
+    .ref('views')
+    .child(video.uid)
+    .once('value');
+  const viewCount = Object.keys(videoViewData.val()).length;
 
-    await database().ref('posts').child(video.user.uid).child(video.uid).child('view').set(viewCount);
+  await database()
+    .ref('posts')
+    .child(video.user.uid)
+    .child(video.uid)
+    .child('view')
+    .set(viewCount);
 
-    return viewCount;
+  return viewCount;
 }
-
