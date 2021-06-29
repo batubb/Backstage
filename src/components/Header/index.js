10214520879af0654e286bdfa1e2e2290e27ebe4
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-06-29 14:03:35 +0300

    Withdraw Icon added to Profile Screen.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -16,12 +16,15 @@ export default class HeaderComponent extends React.Component {
       borderColor,
       backgroundColor,
       rightButtonIcon,
+      rightSecondButtonIcon,
       placement,
       leftButtonIcon,
       title,
       rightButtonPress,
+      rightSecondButtonPress,
       leftButtonPress,
       rightButtonColor,
+      rightSecondButtonColor,
     } = this.props;
 
     return (
@@ -54,17 +57,30 @@ export default class HeaderComponent extends React.Component {
           justifyContent: 'center',
         }}
         rightComponent={
-          rightButtonPress ? (
-            <TouchableOpacity
-              style={styles.rightIcon}
-              onPress={() => rightButtonPress()}>
-              <Icon
-                name={rightButtonIcon}
-                color={rightButtonColor ? rightButtonColor : '#FFF'}
-                type="material-community"
-              />
-            </TouchableOpacity>
-          ) : null
+          <View style={{flexDirection: 'row'}}>
+            {rightSecondButtonPress ? (
+              <TouchableOpacity
+                style={styles.rightIcon}
+                onPress={() => rightSecondButtonPress()}>
+                <Icon
+                  name={rightSecondButtonIcon}
+                  color={rightSecondButtonColor ? rightSecondButtonColor : '#FFF'}
+                  type="material-community"
+                />
+              </TouchableOpacity>
+            ) : null}
+            {rightButtonPress ? (
+              <TouchableOpacity
+                style={styles.secondRightIcon}
+                onPress={() => rightButtonPress()}>
+                <Icon
+                  name={rightButtonIcon}
+                  color={rightButtonColor ? rightButtonColor : '#FFF'}
+                  type="material-community"
+                />
+              </TouchableOpacity>
+            ) : null}
+          </View>
         }
         placement={placement}
         containerStyle={{
@@ -87,9 +103,11 @@ export default class HeaderComponent extends React.Component {
 HeaderComponent.propTypes = {
   leftButtonIcon: PropTypes.string,
   rightButtonIcon: PropTypes.string,
+  rightSecondButtonIcon: PropTypes.string,
   title: PropTypes.string,
   leftButtonPress: PropTypes.func,
   rightButtonPress: PropTypes.func,
+  rightSecondButtonPress: PropTypes.func,
   backgroundColor: PropTypes.string,
   borderColor: PropTypes.string,
   placement: PropTypes.string,
@@ -98,9 +116,11 @@ HeaderComponent.propTypes = {
 HeaderComponent.defaultProps = {
   leftButtonIcon: 'star',
   rightButtonIcon: 'star',
+  rightSecondButtonIcon: 'star',
   title: '',
   leftButtonPress: null,
   rightButtonPress: null,
+  rightSecondButtonPress: null,
   backgroundColor: constants.BACKGROUND_COLOR,
   borderColor: '',
   placement: 'center',
@@ -119,6 +139,9 @@ const styles = StyleSheet.create({
   },
   rightIcon: {
     paddingHorizontal: 15,
+  },
+  secondRightIcon: {
+    paddingRight: 7,
   },
   title: {
     fontFamily: Platform.OS === 'ios' ? 'Avenir' : 'sans-serif-condensed',
