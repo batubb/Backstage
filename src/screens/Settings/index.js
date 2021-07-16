9b17ae07599a2891ce2bab82605f6fa3e236cbe6
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-07-16 04:19:17 -0700

    Share my profile link added to settings.

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -9,16 +9,16 @@ import {
   Alert,
   RefreshControl,
   Linking,
+  ActionSheetIOS,
+  Platform,
 } from 'react-native';
 import {observer} from 'mobx-react';
 import {StackActions} from '@react-navigation/native';
-import {Loading, Header, Text, Label} from '../../components';
+import {Loading, Header, Label} from '../../components';
 import {constants} from '../../resources';
 import auth from '@react-native-firebase/auth';
 import Store from '../../store/Store';
-import { sendBirdDisconnect } from '../../services/connectSendbird';
-
-const {width} = Dimensions.get('window');
+import {sendDataAnalytics} from '../../services';
 
 class Home extends Component {
   constructor(props) {
@@ -60,7 +60,6 @@ class Home extends Component {
           auth()
             .signOut()
             .then(async () => {
-              await sendBirdDisconnect();
               this.goTo('Welcome');
             })
             .catch(() => {
@@ -71,6 +70,32 @@ class Home extends Component {
       },
       {text: 'No', style: 'cancel'},
     ]);
+  };
+
+  shareProfileLink = () => {
+    if (Platform.OS === 'ios') {
+      ActionSheetIOS.showShareActionSheetWithOptions(
+        {
+          message: constants.APP_WEBSITE + '/' + Store.user.username,
+        },
+        (error) =>
+          sendDataAnalytics(
+            'share-profile-link',
+            'error',
+            error,
+            error.name,
+          ),
+        (success, method) =>
+          method
+            ? sendDataAnalytics(
+                'share-profile-link',
+                'success',
+                {method},
+                method,
+              )
+            : null,
+      );
+    }
   };
 
   render() {
@@ -100,6 +125,12 @@ class Home extends Component {
             refreshControl={
               <RefreshControl refreshing={refreshing} tintColor="white" />
             }>
+            <Label
+              text="My Profile Link"
+              icon="share-variant"
+              onPressFunction={() => this.shareProfileLink()}
+              border
+            />
             <Label
               text="Earnings"
               icon="currency-usd"
