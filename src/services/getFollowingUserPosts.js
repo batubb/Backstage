0e651891db8d6899de605cf1d9d3957580559405
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-07-23 05:13:23 -0700

    Admin access to lives and posts in home page.

--- src/services/getFollowingUserPosts.js
+++ src/services/getFollowingUserPosts.js
@@ -1,23 +1,29 @@
 /* eslint-disable prettier/prettier */
 import axios from 'axios';
+import Store from '../store/Store';
 
-export default async function getFollowingUserPosts(uid, followList) {
-    var data = JSON.stringify({ followList });
+export default async function getFollowingUserPosts(
+  uid,
+  followList,
+  userType = Store.user.type,
+) {
+  var data = JSON.stringify({followList, userType});
 
-    var config = {
-        method: 'post',
-        url: 'https://us-central1-backstage-ceb27.cloudfunctions.net/api/getFollowingUserPosts',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        data: data,
-    };
+  var config = {
+    method: 'post',
+    url:
+      'https://us-central1-backstage-ceb27.cloudfunctions.net/api/getFollowingUserPosts',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
 
-    try {
-        const result = await axios(config);
-        return result.data;
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
+  try {
+    const result = await axios(config);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
--- src/services/getFollowingUserStories.js
+++ src/services/getFollowingUserStories.js
@@ -1,23 +1,29 @@
 /* eslint-disable prettier/prettier */
 import axios from 'axios';
+import Store from '../store/Store';
 
-export default async function getFollowingUserStories(uid, followList) {
-    var data = JSON.stringify({ followList, uid });
+export default async function getFollowingUserStories(
+  uid,
+  followList,
+  userType = Store.user.type,
+) {
+  var data = JSON.stringify({followList, uid, userType});
 
-    var config = {
-        method: 'post',
-        url: 'https://us-central1-backstage-ceb27.cloudfunctions.net/api/getFollowingUserStories',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        data: data,
-    };
+  var config = {
+    method: 'post',
+    url:
+      'https://us-central1-backstage-ceb27.cloudfunctions.net/api/getFollowingUserStories',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
 
-    try {
-        const result = await axios(config);
-        return result.data;
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
+  try {
+    const result = await axios(config);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
