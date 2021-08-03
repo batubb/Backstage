6b6ef3665cbe522fc3fb38e4ee11eb4af89456ef
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-08-03 12:07:16 +0300

    Eslint configurated.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    'react/no-did-mount-set-state': 0,
+    'react-native/no-inline-styles': 0,
+    'radix': 0,
+    'no-shadow': 0,
+  },
 };
