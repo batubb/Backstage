6e473fbd8994917107f0fb156f1887f394d2e0a5
Oguzhan <oguzhnatly@gmail.com>
2021-08-06 15:12:38 +0300

    RN in-app purchase billing added to android and ready to release.

--- android/app/src/main/java/com/cooldigital/backstage/MainApplication.java
+++ android/app/src/main/java/com/cooldigital/backstage/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.dooboolab.RNIap.RNIapPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 import com.facebook.react.ReactInstanceManager;
